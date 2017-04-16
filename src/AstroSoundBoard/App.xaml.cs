f54aaa10b9de2fa2068b94492c973e0ecd9b8749
Patrick Hollweck <patrick-hollweck@gmx.de>
2017-04-16 23:10:07 +0200

    Added fody

--- src/AstroSoundBoard/App.xaml.cs
+++ src/AstroSoundBoard/App.xaml.cs
@@ -1,7 +1,7 @@
 ﻿// ****************************** Module Header ****************************** //
 //
 //
-// Last Modified: 16:04:2017 / 22:51
+// Last Modified: 16:04:2017 / 23:03
 // Creation: 16:04:2017
 // Project: AstroSoundBoard
 //
@@ -11,6 +11,8 @@
 
 namespace AstroSoundBoard
 {
+	using System;
+	using System.Reflection;
 	using System.Windows;
 
 	using AstroSoundBoard.WPF.Pages.Settings;
@@ -21,11 +23,16 @@ namespace AstroSoundBoard
 
 	public partial class App : Application
 	{
-		private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
 		private void Application_Startup(object sender, StartupEventArgs e)
 		{
 			Log.Info("--- APP START! ---");
+			Log.Info($"Current Version: {Assembly.GetExecutingAssembly().GetName().Version}");
+
+			// Setup error handling to log fatal errors.
+			AppDomain currentDomain = AppDomain.CurrentDomain;
+			currentDomain.UnhandledException += (caller, args) => { Log.Fatal($"Fatal unhanded exception. - {args.ExceptionObject} -- {args.IsTerminating} -> {args}"); };
 
 			ApplyMaterialTheme();
 		}
