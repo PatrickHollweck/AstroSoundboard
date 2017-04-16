dddb51480bfb2dd9b698f65a32681eedc167cdc2
Patrick Hollweck <patrick-hollweck@gmx.de>
2017-04-16 20:35:56 +0200

    log4net integration

--- src/AstroSoundBoard/App.xaml.cs
+++ src/AstroSoundBoard/App.xaml.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿// ****************************** Module Header ****************************** //
+//
+//
+// Last Modified: 16:04:2017 / 20:35
+// Creation: 16:04:2017
+// Project: AstroSoundBoard
+//
+//
+// <copyright file="App.xaml.cs" company="Patrick Hollweck" GitHub="https://github.com/FetzenRndy">//</copyright>
+// *************************************************************************** //
 
 namespace AstroSoundBoard
 {
-	/// <summary>
-	/// Interaktionslogik für "App.xaml"
-	/// </summary>
+	using System.Windows;
+
+	using log4net;
+
 	public partial class App : Application
 	{
+		private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			Log.Info("--- APP START! ---");
+		}
+
+		private void Application_Exit(object sender, ExitEventArgs e)
+		{
+			Log.Info("--- APP EXIT! ---");
+		}
 	}
 }
--- src/AstroSoundBoard/Properties/AssemblyInfo.cs
+++ src/AstroSoundBoard/Properties/AssemblyInfo.cs
@@ -1,12 +1,19 @@
-﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
+﻿// ****************************** Module Header ****************************** //
+//
+//
+// Last Modified: 16:04:2017 / 20:22
+// Creation: 16:04:2017
+// Project: AstroSoundBoard
+//
+//
+// <copyright file="AssemblyInfo.cs" company="Patrick Hollweck" GitHub="https://github.com/FetzenRndy">//</copyright>
+// *************************************************************************** //
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die einer Assembly zugeordnet sind.
+// .NET
 [assembly: AssemblyTitle("AstroSoundBoard")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -16,40 +23,15 @@ using System.Windows;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
-// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
-// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+// COM
 [assembly: ComVisible(false)]
 
-//Um mit dem Erstellen lokalisierbarer Anwendungen zu beginnen, legen Sie
-//<UICulture>ImCodeVerwendeteKultur</UICulture> in der .csproj-Datei
-//in einer <PropertyGroup> fest.  Wenn Sie in den Quelldateien beispielsweise Deutsch
-//(Deutschland) verwenden, legen Sie <UICulture> auf \"de-DE\" fest.  Heben Sie dann die Auskommentierung
-//des nachstehenden NeutralResourceLanguage-Attributs auf.  Aktualisieren Sie "en-US" in der nachstehenden Zeile,
-//sodass es mit der UICulture-Einstellung in der Projektdatei übereinstimmt.
+// WPF
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
-[assembly: ThemeInfo(
-	ResourceDictionaryLocation.None, //Speicherort der designspezifischen Ressourcenwörterbücher
-									 //(wird verwendet, wenn eine Ressource auf der Seite nicht gefunden wird,
-									 // oder in den Anwendungsressourcen-Wörterbüchern nicht gefunden werden kann.)
-	ResourceDictionaryLocation.SourceAssembly //Speicherort des generischen Ressourcenwörterbuchs
-											  //(wird verwendet, wenn eine Ressource auf der Seite nicht gefunden wird,
-											  // designspezifischen Ressourcenwörterbuch nicht gefunden werden kann.)
-)]
-
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
+// VERSION
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// LOG4NET
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
