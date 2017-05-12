68b738b86c9f21c9175034eb9b8a333bc15ac440
Patrick Hollweck <patrick-hollweck@gmx.de>
2017-05-12 20:16:52 +0200

    Better error handling

--- src/AstroSoundBoard/App.xaml.cs
+++ src/AstroSoundBoard/App.xaml.cs
@@ -1,8 +1,8 @@
 ﻿// ****************************** Module Header ****************************** //
 //
 //
-// Last Modified: 01:05:2017 / 01:21
-// Creation: 29:04:2017
+// Last Modified: 12:05:2017 / 20:16
+// Creation: 11:05:2017
 // Project: AstroSoundBoard
 //
 //
@@ -21,11 +21,15 @@ namespace AstroSoundBoard
     using AstroSoundBoard.Core.Utils;
     using AstroSoundBoard.Properties;
 
+    using CrashReporterDotNET;
+
     using log4net;
     using log4net.Core;
     using log4net.Repository.Hierarchy;
 
     using MaterialDesignThemes.Wpf;
+
+    using SharpRaven;
 
     public partial class App : Application
     {
@@ -48,25 +52,42 @@ namespace AstroSoundBoard
             FileSystem.FolderHelper.CreateIfMissing($"{AppSettings.InstallationFilePath}/");
 
             // Setup error handling to log fatal errors.
-            AppDomain.CurrentDomain.UnhandledException += (caller, args) =>
-                {
-                    Log.Fatal($"Fatal unhanded exception. - {args.ExceptionObject} -- {args.IsTerminating} -> {args}");
-
 #if !DEBUG
-                    if (AstroSoundBoard.Properties.Settings.Default.AllowErrorReporting)
-                    {
-                        var ravenClient = new RavenClient(Credentials.SentryApiKey);
-                        ravenClient.Capture(new SharpRaven.Data.SentryEvent((Exception)args.ExceptionObject));
-
-                        Log.Info("Reported error to sentry!");
-                    }
-
+            AppDomain.CurrentDomain.UnhandledException += ReportError;
 #endif
-                };
 
             ApplyMaterialTheme();
             SoundManager.Init();
             SettingsManager.Init();
+        }
+
+        public static void ReportError(object sender, UnhandledExceptionEventArgs args)
+        {
+            Log.Fatal($"Fatal unhanded exception. - {args.ExceptionObject} -- {args.IsTerminating} -> {args}");
+
+            if (Settings.Default.AllowErrorReporting)
+            {
+                var ravenClient = new RavenClient(Credentials.SentryApiKey);
+                ravenClient.Capture(new SharpRaven.Data.SentryEvent((Exception)args.ExceptionObject));
+
+                ReportCrash((Exception)args.ExceptionObject);
+
+                Log.Info("Reported error to sentry!");
+            }
+        }
+
+        public static void ReportCrash(Exception exception, string developerMessage = "")
+        {
+            var reportCrash = new ReportCrash
+            {
+                IncludeScreenshot = true,
+                CaptureScreen = true,
+
+                DeveloperMessage = developerMessage,
+                ToEmail = "patrick-hollweck@gmx.de"
+            };
+
+            reportCrash.Send(exception);
         }
 
         private void Application_Exit(object sender, ExitEventArgs e)
