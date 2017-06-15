6379d39daa1d8e07f9a49baba417079c6e82b32b
Patrick Hollweck <patrick-hollweck@gmx.de>
2017-06-15 16:21:51 +0200

    Updated Api keys

--- src/AstroSoundBoard/Core/Objects/Credentials.cs
+++ src/AstroSoundBoard/Core/Objects/Credentials.cs
@@ -12,10 +12,10 @@
 namespace AstroSoundBoard.Core.Objects
 {
     /// <summary>
-    /// Credentials and API Keys (I dunno how to not put this on git without my git client complaining so, here you go with my api keys, I know this is a security Problem. Please dont abuse me.)
+    /// Credentials and API Keys
     /// </summary>
     public class Credentials
     {
-        public const string SentryApiKey = "https://7022c1d1e36c4342882fd531a437f87d:17f4fdd9922148558ecd41f69f6b70f3@sentry.io/162665";
+        public const string SentryApiKey = "https://7022c1d1e36c4342882fd531a437f87d@sentry.io/162665";
     }
 }
