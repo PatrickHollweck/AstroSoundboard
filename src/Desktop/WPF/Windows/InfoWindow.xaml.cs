a7e05d6394f99252dde79a0c998766f917014ae6
Patrick Hollweck <patrick-hollweck@gmx.de>
2017-07-04 21:10:00 +0200

    Fixed a bug when trying to save a Sound

--- src/Desktop/WPF/Windows/InfoWindow.xaml.cs
+++ src/Desktop/WPF/Windows/InfoWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿// ****************************** Module Header ****************************** //
 //
 //
-// Last Modified: 01:07:2017 / 11:45
+// Last Modified: 04:07:2017 / 20:59
 // Creation: 01:07:2017
 // Project: AstroSoundBoard
 //
@@ -13,11 +13,13 @@ namespace AstroSoundBoard.WPF.Windows
 {
     using System.Diagnostics;
     using System.IO;
+    using System.Net;
     using System.Windows;
 
     using AstroSoundBoard.Core.Components;
     using AstroSoundBoard.Core.Objects;
     using AstroSoundBoard.Core.Objects.Models;
+    using AstroSoundBoard.Core.Utils.Extensions;
 
     using Microsoft.Win32;
 
@@ -26,20 +28,42 @@ namespace AstroSoundBoard.WPF.Windows
 
     public partial class InfoWindow : Window
     {
-        public SoundModel LocalSoundModel { get; set; }
+        public SoundModel Model { get; set; }
 
         public InfoWindow(SoundModel def)
         {
-            LocalSoundModel = def;
+            Model = def;
             InitializeComponent();
             DataContext = this;
         }
 
-        private void SaveSound(object sender, RoutedEventArgs e)
+        private async void SaveSound(object sender, RoutedEventArgs e)
         {
             // Get the audio stream ( file from resources )
-            LocalSoundModel.Name = LocalSoundModel.Name.Replace(" ", "_");
-            var soundStream = (UnmanagedMemoryStream)SoundManager.GetAudioFileFromResources(LocalSoundModel.Name);
+            var soundStream = (UnmanagedMemoryStream)SoundManager.GetAudioFileFromResources(Model.Name.ToFileName());
+
+            if (!File.Exists($"{AppSettings.AssemblyDirectory}/libmp3lame.32.dll"))
+            {
+                var result = MessageBox.Show("A library is required to be able to save Sounds. If you click OK the library will be downloaded automatically, and you will be able to save the Sound.", "Notification", MessageBoxButton.OKCancel, MessageBoxImage.Asterisk);
+
+                if (result == MessageBoxResult.OK)
+                {
+                    try
+                    {
+                        using (WebClient wc = new WebClient())
+                        {
+                            await wc.DownloadFileTaskAsync(@"http://www.github.com/FetzenRndy/AstroSoundboard/raw/develop/src/Desktop/libmp3lame.32.dll", "libmp3lame.32.dll");
+                        }
+
+                        MessageBox.Show("Download Completed! \nNow there is a file named libmp3lame.32.dll in the directory, you started the Programm from, when you are done saving sounds, you can delete this file savely again! \nNow you can Save Sounds :) Have fun.", "Success", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Error, while downloading!");
+                        return;
+                    }
+                }
+            }
 
             // Save it to the file if the getting was successful, yes getting, thats absolutely correct
             if (soundStream != null)
@@ -49,8 +73,8 @@ namespace AstroSoundBoard.WPF.Windows
                 {
                     InitialDirectory = $"{AppSettings.AssemblyDirectory}",
                     Filter = ".mp3 File (*.mp3)|*.mp3",
-                    Title = $"Sound Location for sound : {LocalSoundModel.Name}",
-                    FileName = LocalSoundModel.Name
+                    Title = $"Sound Location for sound : {Model.Name}",
+                    FileName = Model.Name
                 };
 
                 if (dialog.ShowDialog() != true)
