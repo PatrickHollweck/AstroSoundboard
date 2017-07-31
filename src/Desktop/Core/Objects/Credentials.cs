53c3ae33f7efdefcbd64f7b6ea0d5c3f7f962103
Patrick Hollweck <patrick-hollweck@gmx.de>
2017-07-31 23:11:26 +0200

    Updated Credentials

--- src/Desktop/Core/Objects/Credentials.cs
+++ src/Desktop/Core/Objects/Credentials.cs
@@ -19,6 +19,6 @@ namespace AstroSoundBoard.Core.Objects
         /// <summary>
         /// Public Api key for the Sentry Error reporting system.
         /// </summary>
-        public const string SentryApiKey = "https://7022c1d1e36c4342882fd531a437f87d@sentry.io/162665";
+        public const string SentryApiKey = "https://7022c1d1e36c4342882fd531a437f87d:17f4fdd9922148558ecd41f69f6b70f3@sentry.io/162665";
     }
 }
